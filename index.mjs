9b9269c812be926b83d19d3bcdf57fecaf79c4f9
Michael Sprague <michael@sprague.com>
2025-09-16 10:00:04 -0700

    deployment

--- index.mjs
+++ index.mjs
@@ -8,7 +8,7 @@ import https from "https";
 import child_process from "child_process";
 import ini from 'ini';
 import { Epistery } from 'epistery';
-import { Certify } from '@metric-im/administrate';
+import { Certify, Deplyment } from '@metric-im/administrate';
 
 const rootDomain = 'thirdparty.company';
 
@@ -23,6 +23,8 @@ async function main() {
   // await epistery.attach(app);
 
   const certify = await Certify.attach(app,{contactEmail:'michael@sprague.com'});
+
+  const deployment = await Deplyment.attach(app,undefined,'main');
 
   // fetch the repo readme and page template to use as the home page
   const template = (fs.readFileSync(resolve('./index.html'))).toString();
