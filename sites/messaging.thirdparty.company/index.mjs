198e1cfedc530bde4cfe980f07eed53537fff492
michael <michael@sprague.com>
2025-09-15 22:51:37 -0400

    spawns

--- sites/messaging.thirdparty.company/index.mjs
+++ sites/messaging.thirdparty.company/index.mjs
@@ -2,6 +2,9 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { Epistery } from 'epistery';
+import https from "https";
+import http from "http";
+import { Certify} from '@metric-im/administrate';
 
 // Get directory paths for ES modules
 const __filename = fileURLToPath(import.meta.url);
@@ -13,6 +16,7 @@ async function main() {
   // attach Epistery directly to app
   const epistery = await Epistery.connect();
   // await epistery.attach(app);
+  const certify = await Certify.attach(app,{contactEmail:'michael@sprague.com'});
 
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -25,6 +29,23 @@ async function main() {
     console.log(`Test site: http://localhost:${PORT}`);
     console.log(`Status: http://localhost:${PORT}/.epistery/status`);
   });
+  const http_server = http.createServer(app);
+  http_server.listen(PORT);
+  http_server.on('error', console.error);
+  http_server.on('listening',()=>{
+    let address = http_server.address();
+    console.log(`Listening on ${address.address} ${address.port} (${address.family})`);
+    console.log(`Test site: http://localhost:${PORT}`);
+  });
+  const https_server = https.createServer(certify.SNI,app);
+  https_server.listen(PORTSSL);
+  https_server.on('error', console.error);
+  https_server.on('listening',()=>{
+    let address = https_server.address();
+    console.log(`Listening on ${address.address} ${address.port} (${address.family})`);
+    console.log(`Test site: http://localhost:${PORT}`);
+  });
+
 }
 
 main().catch(err => {
